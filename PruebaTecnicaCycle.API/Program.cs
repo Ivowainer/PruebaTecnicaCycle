fb020af1d904e15fe0a07311750237cb0646b0a2
Ivowainer <ivancamposwainer@gmail.com>
2024-04-02 14:38:57 -0300

    Readme.MD

--- PruebaTecnicaCycle.API/Program.cs
+++ PruebaTecnicaCycle.API/Program.cs
@@ -69,6 +69,7 @@ builder.Services.AddSingleton<IErrorHandler, ErrorHandler>();
 
 builder.Services.AddCors(options =>
 {
+    // ONLY FOR TESTING
     options.AddPolicy("new", app =>
     {
         app.AllowAnyOrigin();
